0fe179084d57d1f3c86ad86b8f883025b03946dd
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:18:06 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,43 @@
+"use strict";
+// TRUE
+// 1
+let car = 'audi';
+console.log("Is car == 'audi'? I predict true.");
+console.log(car == 'audi');
+// 2
+let phone = 'vivo';
+console.log("Is phone == 'vivo'? I predict true.");
+console.log(phone == 'vivo');
+// 3
+let carSecond = 'mercedes';
+console.log("Is carSecond == 'mercedes'? I predict true.");
+console.log(carSecond == 'mercedes');
+// 4
+let phoneSecond = 'iphone';
+console.log("Is phoneSecond == 'iphone'? I predict true.");
+console.log(phoneSecond == 'iphone');
+// 5
+let carTire = '4';
+console.log("Is carTire == '4'? I predict true.");
+console.log(carTire == '4');
+//FALSE
+// 6
+let pc = 'dell';
+console.log("Is pc > 'dell'? I predict false.");
+console.log(pc > 'dell');
+// 7
+let graphicCard = 'nvidia';
+console.log("Is graphicCard > 'nvidia'? I predict false.");
+console.log(graphicCard > 'nvidia');
+// 8
+let ram = '8gb';
+console.log("Is ram > '8gb'? I predict false.");
+console.log(ram > '8gb');
+// 9
+let carDoor = '6';
+console.log("Is carDoor > '6'? I predict false.");
+console.log(carDoor > '6');
+// 10
+let monitor = 'asus';
+console.log("Is monitor > 'asus'? I predict false.");
+console.log(monitor > 'asus');
